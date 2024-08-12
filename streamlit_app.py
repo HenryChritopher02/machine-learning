842360c4945e069717b5252198e944758af6e338
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-12 15:29:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,35 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from mordred import Calculator, descriptors
+from sklearn.preprocessing import StandardScaler
 
 st.title('🎈 Machine learning App')
 
 st.write('Hello world!')
 
-df = pd.read_csv(r'https://raw.githubusercontent.com/HenryChritopher02/bace1/main/data/bace1_standardized.csv')
-df = df.drop(df.columns[0], axis=1)
-df
+with st.expand('Data'):
+  st.write('**Standardized data**')
+  data = pd.read_csv(r'https://raw.githubusercontent.com/HenryChritopher02/bace1/main/data/bace1_standardized.csv')
+  data = data.drop(df.columns[0], axis=1)
+  data
+
+  st.write('**Calculated descriptors data**')
+
+  def All_Mordred_descriptors(smiles_data):
+      calc = Calculator(descriptors, ignore_3D=True)
+      mols = [Chem.AddHs(Chem.MolFromSmiles(smi)) for smi in tqdm(smiles_data, desc='Adding Hydrogens')]
+      
+      # pandas df
+      df = calc.pandas(mols)
+      return df
+  
+  data_des = All_Mordred_descriptors(data['standardized_smiles'])
+  data_des = data_des.apply(pd.to_numeric, errors='coerce')
+  data_des.dropna(axis=1, inplace=True)
+  scaler = StandardScaler()
+  data_des_scaled = scaler.fit_transform(data_des)
+  data_des = pd.DataFrame(data_des_scaled, columns=data_des.columns)
+  data_des = data_des.astype('float64')
+  total = pd.concat([data['pIC50'], data_des], axis=1)
+  total
