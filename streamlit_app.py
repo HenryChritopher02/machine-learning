39023e93800cbaf98c923aae684c64b5fd2a4223
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-12 15:49:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,22 +18,22 @@ with st.expander('Data'):
   data = data.drop(data.columns[0], axis=1)
   data
 
-  st.write('**Calculated descriptors data**')
+  # st.write('**Calculated descriptors data**')
 
-  def All_Mordred_descriptors(smiles_data):
-      calc = Calculator(descriptors, ignore_3D=True)
-      mols = [Chem.AddHs(Chem.MolFromSmiles(smi)) for smi in tqdm(smiles_data, desc='Adding Hydrogens')]
+  # def All_Mordred_descriptors(smiles_data):
+  #     calc = Calculator(descriptors, ignore_3D=True)
+  #     mols = [Chem.AddHs(Chem.MolFromSmiles(smi)) for smi in tqdm(smiles_data, desc='Adding Hydrogens')]
       
-      # pandas df
-      df = calc.pandas(mols)
-      return df
+  #     # pandas df
+  #     df = calc.pandas(mols)
+  #     return df
   
-  data_des = All_Mordred_descriptors(data['standardized_smiles'])
-  data_des = data_des.apply(pd.to_numeric, errors='coerce')
-  data_des.dropna(axis=1, inplace=True)
-  scaler = StandardScaler()
-  data_des_scaled = scaler.fit_transform(data_des)
-  data_des = pd.DataFrame(data_des_scaled, columns=data_des.columns)
-  data_des = data_des.astype('float64')
-  total = pd.concat([data['pIC50'], data_des], axis=1)
-  total
+  # data_des = All_Mordred_descriptors(data['standardized_smiles'])
+  # data_des = data_des.apply(pd.to_numeric, errors='coerce')
+  # data_des.dropna(axis=1, inplace=True)
+  # scaler = StandardScaler()
+  # data_des_scaled = scaler.fit_transform(data_des)
+  # data_des = pd.DataFrame(data_des_scaled, columns=data_des.columns)
+  # data_des = data_des.astype('float64')
+  # total = pd.concat([data['pIC50'], data_des], axis=1)
+  # total
