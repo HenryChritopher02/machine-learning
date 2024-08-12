0442f1d7898905f1639c0b0123a6ac7702204565
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-12 16:06:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import rdkit
 from rdkit import Chem, RDLogger
-from mordred import Calculator, descriptors
+from rdkit.ML.Descriptors import MoleculeDescriptors
+from rdkit.Chem import Descriptors, AllChem
 from sklearn.preprocessing import StandardScaler
 
 # RDLogger.DisableLog('rdApp.*')
@@ -18,22 +19,30 @@ with st.expander('Data'):
   data = data.drop(data.columns[0], axis=1)
   data
 
-  # st.write('**Calculated descriptors data**')
+  st.write('**Calculated descriptors data**')
 
-  # def All_Mordred_descriptors(smiles_data):
-  #     calc = Calculator(descriptors, ignore_3D=True)
-  #     mols = [Chem.AddHs(Chem.MolFromSmiles(smi)) for smi in tqdm(smiles_data, desc='Adding Hydrogens')]
-      
-  #     # pandas df
-  #     df = calc.pandas(mols)
-  #     return df
-  
-  # data_des = All_Mordred_descriptors(data['standardized_smiles'])
-  # data_des = data_des.apply(pd.to_numeric, errors='coerce')
-  # data_des.dropna(axis=1, inplace=True)
-  # scaler = StandardScaler()
-  # data_des_scaled = scaler.fit_transform(data_des)
-  # data_des = pd.DataFrame(data_des_scaled, columns=data_des.columns)
-  # data_des = data_des.astype('float64')
-  # total = pd.concat([data['pIC50'], data_des], axis=1)
-  # total
+  def rdkit_descriptors(smiles):
+      mols = [Chem.MolFromSmiles(i) for i in smiles]
+      calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0]
+                                       for x in Descriptors._descList])
+      desc_names = calc.GetDescriptorNames()
+    
+      mol_descriptors =[]
+      for mol in mols:
+        #Add hydrogens to molecules
+        mol=Chem.AddHs(mol)
+        #Calculate all 200 descriptors for each molecule
+        descriptors = calc.CalcDescriptors(mol)
+        mol_descriptors.append(descriptors)
+      return mol_descriptors, desc_names
+
+  mol_descriptors, desc_names = rdkit_descriptors(data['standardized_smiles'])
+  data_des = pd.DataFrame(mol_descriptors,columns=desc_names)
+  data_des = data_des.apply(pd.to_numeric, errors='coerce')
+  data_des.dropna(axis=1, inplace=True)
+  scaler = StandardScaler()
+  data_des_scaled = scaler.fit_transform(data_des)
+  data_des = pd.DataFrame(data_des_scaled, columns=data_des.columns)
+  data_des = data_des.astype('float64')
+  total = pd.concat([data['pIC50'], data_des], axis=1)
+  total
