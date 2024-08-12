ba6b4e5e3646962c706fc6efda6a4e4b74b1500a
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-12 15:51:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import rdkit
-# from rdkit import Chem, RDLogger
-# from mordred import Calculator, descriptors
+from rdkit import Chem, RDLogger
+from mordred import Calculator, descriptors
 # from sklearn.preprocessing import StandardScaler
 
 # RDLogger.DisableLog('rdApp.*')
