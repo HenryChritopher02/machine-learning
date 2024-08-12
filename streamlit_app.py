c68d9250bfab72f8eb8f11b736a12e1d18b5c2fa
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-12 15:17:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ import numpy as np
 st.title('🎈 Machine learning App')
 
 st.write('Hello world!')
+
+df = pd.read_csv(r'https://raw.githubusercontent.com/HenryChritopher02/bace1/main/data/bace1_standardized.csv',index=False)
+df
