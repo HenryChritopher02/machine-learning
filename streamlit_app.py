7086505d2a5fe6b34d9fdc124f9054efec3a1ec7
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-13 11:06:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,6 @@ with st.expander('Input'):
     option = st.radio("Choose an option", ("Upload CSV file", "Input SMILES string"), index=None)
     if option == "Upload CSV file":
         uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-        st.stop()  # Stop here and wait for user input
         if uploaded_file is not None:
             @st.cache_data
             def process_csv(uploaded_file):
@@ -132,7 +131,6 @@ with st.expander('Input'):
 
     elif option == "Input SMILES string":
         smiles_input = st.text_input("Enter a SMILES string")
-        st.stop()  # Stop here and wait for user input
         data = pd.DataFrame({'SMILES': [smiles_input]})
         standardized_smiles, invalid_smiles = standardize_smiles(data['SMILES'])
         st.write('Standardized SMILES:')
