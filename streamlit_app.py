3a421370d4c330425852ebb5afd6f175ff256024
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2024-08-15 12:39:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,9 +71,9 @@ def rdkit_descriptors(smiles):
 #   st.write('**Standardized data**')
 data = pd.read_csv('https://raw.githubusercontent.com/HenryChritopher02/bace1/main/data/bace1_standardized.csv')
 data = data.drop(data.columns[0], axis=1)
-data
+# data
 
-st.write('**Calculated descriptors data**')
+# st.write('**Calculated descriptors data**')
 mol_descriptors, desc_names = rdkit_descriptors(data['standardized_smiles'])
 data_des = pd.DataFrame(mol_descriptors,columns=desc_names)
 data_des = data_des.apply(pd.to_numeric, errors='coerce')
@@ -81,15 +81,15 @@ data_des.dropna(axis=1, inplace=True)
 columns = data_des.columns
 data_des = data_des.astype('float64')
 total = pd.concat([data['pIC50'], data_des], axis=1)
-total
+# total
 
 # st.write('**X**')
 X = total.drop('pIC50', axis=1)
-X
+# X
   
 # st.write('**y**')
 y = total['pIC50']
-y
+# y
   
 with st.expander('Input'):
     option = st.radio("Choose an option", ("Upload CSV file", "Input SMILES string"), index=None)
