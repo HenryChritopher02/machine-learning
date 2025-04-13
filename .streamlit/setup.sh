c11b4fd17ec75689fee63c80bc03d88deebe938b
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2025-04-13 18:55:43 +0700

    Update setup.sh

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-chmod +x "$(pwd)/vina/vina_1.2.5_linux_x86_64"
+git update-index --chmod=+x vina/vina_1.2.5_linux_x86_64
+git commit -am "Mark Vina binary as executable"
+git push
 ls -l ./vina/vina_1.2.5_linux_x86_64  # Debug: Check permissions
