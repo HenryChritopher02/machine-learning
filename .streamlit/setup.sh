bd418e0cc13bedbe1bb41221c4360cedd1adbd81
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2025-04-13 19:30:47 +0700

    Update setup.sh

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-chmod +x ./vina/vina_1.2.5_linux_x86_64
-ls -l ./vina/vina_1.2.5_linux_x86_64
+wget https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v1.2.5/vina_1.2.5_linux_x86_64
+mv vina_1.2.5_linux_x86_64 vina
+chmod 755 -R ./vina
