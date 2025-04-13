15aad11f9fa3184eba5fed2391f530e180e06bf2
KyNguyen <118060564+HenryChritopher02@users.noreply.github.com>
2025-04-13 18:25:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ import os
 from io import BytesIO
 from sklearn.exceptions import InconsistentVersionWarning
 import warnings
+import subprocess
 
 # Suppress the InconsistentVersionWarning
 warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
