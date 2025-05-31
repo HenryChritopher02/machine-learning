db15246e523662cd5053fd4b5a7c80c96eff205a
Song-Ky Nguyen-Vo <118060564+HenryChritopher02@users.noreply.github.com>
2025-05-31 11:35:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # .streamlit/setup.sh
 chmod +x ./vina/vina_1.2.5_linux_x86_64
+chmod +x ./ensemble_docking/Vina_screenin.pl
+chmod +x ./ensemble_docking/ligand_preprocessing/mk_prepare_ligand.py
+chmod +x ./ensemble_docking/ligand_preprocessing/scrub.py
